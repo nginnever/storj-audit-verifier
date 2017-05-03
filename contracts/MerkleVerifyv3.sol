0a16fc27dc8ec6e5d5cc4a3a77e9c7e35008872f
nginnever <ginneversource@gmail.com>
2017-05-02 19:38:11 -0700

    fix index

--- contracts/MerkleVerifyv3.sol
+++ contracts/MerkleVerifyv3.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.4.8;
 
 contract MerkleVerifyv2 {
-
+  
   event auditEvent(bool returnValue);
   function audit(uint index, bytes32 rootHash, bytes proof) returns (bool) {
     // use the index to determine the node ordering
     // index ranges 1 to n
     
-    uint i = index;
     bytes32 tempHash;
     bytes32 preImage;
     
@@ -20,14 +19,14 @@ contract MerkleVerifyv2 {
         tempHash := mload(add(proof, j))
       }
       
-      if(i%2 == 1) {
-        preImage = sha3(preImage, tempHash);
-        i = (i/2) + 1;
-      }
-      if(i%2 == 0) {
+      if(index%2 == 0) {
         preImage = sha3(tempHash, preImage);
-        i = i/2;
+        index = index/2;
+      } else {
+        preImage = sha3(preImage, tempHash);
+        index = uint(index)/2 + 1;
       }
+ 
     }
     
     auditEvent(preImage == rootHash);
